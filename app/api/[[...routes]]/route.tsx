15686d6b2bdc2fbe000136d14043761da937f915
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-08-18 13:12:15 -0500

    likebetta

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -1,8 +1,6 @@
 /** @jsxImportSource frog/jsx */
-
-import { Button, Frog, TextInput } from 'frog'
+import { Button, Frog } from 'frog'
 import { devtools } from 'frog/dev'
-// import { neynar } from 'frog/hubs'
 import { handle } from 'frog/next'
 import { serveStatic } from 'frog/serve-static'
 
@@ -10,12 +8,8 @@ const app = new Frog({
   assetsPath: '/',
   basePath: '/api',
   title: 'likebetter',
-  // Supply a Hub to enable frame verification.
-  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' }) // Uncomment and configure if needed
 })
-
-// Uncomment to use Edge Runtime
-// export const runtime = 'edge'
 
 app.frame('/', (c) => {
   return c.res({
@@ -35,8 +29,8 @@ app.frame('/picker', (c) => {
   if (buttonValue === 'A') {
     return c.res({
       action: 'degen/a',
-      image: "https://amaranth-adequate-condor-278.mypinata.cloud/ipfs/QmVti6U1jYrWX8RwvJcNCHoKD6zXToDX9T6kfydH5iaxvY",
-      imageAspectRatio: '1:1',
+      image: "https://amaranth-adequate-condor-278.mypinata.cloud/ipfs/QmVti6U1jYrWX8RwvJcNCHoKD6zXToDX9T6kfydH5iaxvY", // Required image property
+      imageAspectRatio: '1:1', // Optional, but useful
       intents: [
         <Button action="/">Back</Button>,
       ],
@@ -44,16 +38,23 @@ app.frame('/picker', (c) => {
   } else if (buttonValue === 'B') {
     return c.res({
       action: 'farther/b',
-      image: "https://amaranth-adequate-condor-278.mypinata.cloud/ipfs/QmTXUnNBVCJEuT6GYdxK1xrjizL2aJWTMWvo9q9Kx6hYje",
-      imageAspectRatio: '1:1',
+      image: "https://amaranth-adequate-condor-278.mypinata.cloud/ipfs/QmTXUnNBVCJEuT6GYdxK1xrjizL2aJWTMWvo9q9Kx6hYje", // Required image property
+      imageAspectRatio: '1:1', // Optional, but useful
+      intents: [
+        <Button action="/">Back</Button>,
+      ],
+    })
+  } else {
+    // Default response if buttonValue is not A or B
+    return c.res({
+      action: '/',
+      image: "https://amaranth-adequate-condor-278.mypinata.cloud/ipfs/QmcYBuqkV7MRavbb9yg4ZKihP4ZmfPSZNZhBitMuwCNXfa", // Required image property
       intents: [
         <Button action="/">Back</Button>,
       ],
     })
   }
 })
-
-
 
 devtools(app, { serveStatic })
 
