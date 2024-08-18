e1b6301096712aed51a4f61c475b2f1d5ab0765d
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-08-18 15:44:22 -0500

    whichone

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
