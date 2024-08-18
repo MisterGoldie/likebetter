ca76732a0b26e1a73cd16628524017d3f28d4e5d
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-08-18 14:31:12 -0500

    new last

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -15,8 +15,8 @@ app.frame('/', (c) => {
     action: '/picker',
     image: "https://amaranth-adequate-condor-278.mypinata.cloud/ipfs/QmcYBuqkV7MRavbb9yg4ZKihP4ZmfPSZNZhBitMuwCNXfa",
     intents: [
-      <Button value="A">$DEGEN</Button>,
-      <Button value="B">$FARTHER</Button>,
+      <Button value="$DEGEN">$DEGEN</Button>,
+      <Button value="$FARTHER">$FARTHER</Button>,
     ],
   })
 })
@@ -25,7 +25,7 @@ app.frame('/picker', (c) => {
   console.log('c', c)
   const { buttonValue } = c
 
-  if (buttonValue === 'A') {
+  if (buttonValue === '$DEGEN') {
     return c.res({
       action: 'degen/a',
       image: "https://amaranth-adequate-condor-278.mypinata.cloud/ipfs/QmVti6U1jYrWX8RwvJcNCHoKD6zXToDX9T6kfydH5iaxvY", 
@@ -34,7 +34,7 @@ app.frame('/picker', (c) => {
         <Button action="/">Back</Button>,
       ],
     })
-  } else if (buttonValue === 'B') {
+  } else if (buttonValue === '$FARTHER') {
     return c.res({
       action: 'farther/b',
       image: "https://amaranth-adequate-condor-278.mypinata.cloud/ipfs/QmXfxPRruMHcfkpquxqPfvBeeY8Mfcnw8x2LKcs7TY2Jk5", 
